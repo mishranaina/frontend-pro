16ebb302e015da69f912ae1205c6303e92ec34ed
naina <mishranaina.687@gmail.com>
2023-12-25 17:18:04 +0530

    first commit

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ searchIcon.addEventListener('click', function() {
         searchDropdown.style.display = 'none';
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Fetch data from the mock API (replace the URL with your API endpoint)
+    fetch('https://jsonplaceholder.typicode.com/posts/2')
+        .then(response => response.json())
+        .then(data => {
+            // Update product details dynamically
+            const productDetailsSection = document.getElementById('product-details');
+            productDetailsSection.innerHTML = `
+                <p>${data.body}</p>
+            `;
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
